99eb73cc7bc97d4c78c272790142fc231543a5fa
Rohit-1717 <rohitvishwakarma1717@gmail.com>
2024-12-28 19:08:31 +0530

    Login,SignUp,& Home Page Folder Create

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
+import Login from "./pages/login/Login";
+
 function App() {
   return (
-    <>
-      <h1 className="text-red-800 text-center font-extrabold text-5xl">
-        Setup Done
-      </h1>
-    </>
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Login/>
+    </div>
   );
 }
 
