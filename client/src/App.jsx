90d6b7761d58cc9b67ffe0551532ab1b80e1a677
Rohit-1717 <rohitvishwakarma1717@gmail.com>
2024-12-31 18:40:47 +0530

    Client setup complete

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-4xl font-extralight text-red-400">
+        Hello Vite + React!
+      </h1>
+    </>
+  );
+}
+
+export default App;
